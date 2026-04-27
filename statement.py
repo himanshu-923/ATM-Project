1ef32b6fba3bea5c8c020e3e2ccc8fd513885db4
Himanshu Kumar <144578539+himanshu-923@users.noreply.github.com>
2026-04-27 10:34:05 +0530

    Enhance show_statement with account details
    Added account holder and account number details to the statement display.

--- statement.py
+++ statement.py
@@ -3,6 +3,8 @@ from utils import statement
 
 def show_statement():
     print("\nMini Statement")
+    print("\nAccount holder: Himanshu kumar")
+    print("\nAccount Number: 0000 XXXX 0000")
     if len(statement) == 0:
         print("No transaction found")
     else:
